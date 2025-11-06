99ecd71f747ac4350cae2c82294dca5a324a8b42
郝能吃 <3416131190@qq.com>
2025-11-06 13:41:25 +0800

    Initial commit

---
+++ src/test/java/com/anxu/smarthomeunity/SmartHomeUnityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anxu.smarthomeunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHomeUnityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
