e8e14b8b6ad3a82561c1f9066092c2ee9a30d60c
郝安旭 <haoanxu@neusoft.com>
2025-11-18 09:38:59 +0800

    日常更新

--- src/main/java/com/anxu/smarthomeunity/mapper/UserMapper.java
+++ src/main/java/com/anxu/smarthomeunity/mapper/UserMapper.java
@@ -1,12 +1,13 @@
 package com.anxu.smarthomeunity.mapper;
 
 import com.anxu.smarthomeunity.pojo.user.UserInfo;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
-public interface UserMapper {
+public interface UserMapper extends BaseMapper<UserInfo> {
     //    用户注册
-    Integer insert(UserInfo userInfo);
+    int insert(UserInfo userInfo);
     //    根据用户名查询用户信息
     UserInfo queryByUsername(String username);
 }
